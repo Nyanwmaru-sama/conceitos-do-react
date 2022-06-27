d93352130452de727252c32638ce46c082a869e3
Nyanwmaru-sama <76477418+Nyanwmaru-sama@users.noreply.github.com>
2022-06-27 02:29:27 -0300

    Solução para o desafio cap 1 Rocketseat

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const novaTarefa = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, novaTarefa]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const concluirTarefa = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(concluirTarefa);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTarefa = tasks.filter(task => task.id !== id);
+
+    setTasks(removeTarefa);
   }
 
   return (
